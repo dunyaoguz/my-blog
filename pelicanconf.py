f470c26131cdbfd384ce63b42146c2b710a7bafc
Dunya Oguz <dunyaoguz13@gmail.com>
2019-04-29 00:52:29 -0400

    add social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,11 +6,13 @@ import os
 AUTHOR = 'Dunya Oguz'
 SITENAME = 'dunyaoguz.github.io'
 SITEURL = 'https://dunyaoguz.github.io/my-blog'
+TAGLINE = 'Data Science Enthusiast.'
 PATH = 'content'
 STATIC_PATHS = ['images']
 TIMEZONE = 'America/Toronto'
 DEFAULT_LANG = 'en'
 DEFAULT_PAGINATION = 10
+
 # set to False for Production, True for Development
 if os.environ.get('PELICAN_ENV') == 'DEV':
     RELATIVE_URLS = True
